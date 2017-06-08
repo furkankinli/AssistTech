6a42da9b83222ea15053d66921191b9b41d3d1ea
furkankinli <furkan.kinli@ozu.edu.tr>
2017-06-08 18:03:12 +0300

    .

--- AssistTech_Py/AssistTech_Py/AssistTech_Py.py
+++ AssistTech_Py/AssistTech_Py/AssistTech_Py.py
@@ -31,10 +31,10 @@ def main():
 
         if is_first_frame:
             largest_area = 0
-            gaussian_blurred = cv2.GaussianBlur(frame, (7, 7), 0)
+            gaussian_blurred = cv2.GaussianBlur(frame, (7, 7), 0) # Kernel size değişebilir noise robust için
             foreground_mask = mog2_bgs.apply(gaussian_blurred)
             _, thresholded = cv2.threshold(foreground_mask, float(70.0), 255, cv2.THRESH_BINARY)
-            gradient = cv2.morphologyEx(thresholded, cv2.MORPH_OPEN, element)
+            gradient = cv2.morphologyEx(thresholded, cv2.MORPH_OPEN, element)  #sadece dilation a çevrilebilir
             foreground = gradient
 
             image, contours, hierarchy = cv2.findContours(foreground.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -42,10 +42,10 @@ def main():
             for i, cnt in enumerate(contours):
                 area = cv2.contourArea(cnt)
                 tmp_size = np.size(frame)
-                if not ((1000 < area < 3000) or area > tmp_size / 8): # çözülmesi gerekiyor
+                if not ((1000 < area < 3000) or area > tmp_size / 8): # area aralığı çözülmesi gerekiyor
                     if largest_area < area:
                         x, y, w, h = cv2.boundingRect(cnt)
-                        if w > 50 and h > 50: # çözülmesi gerekiyor
+                        if w > 50 and h > 50: # kare bulmamız ve belli bir uzunluktan fazla olması çözülmesi gerekiyor
                             bbox = (x, y, w, w)
                             largest_area = area
                             ok = tracker.init(frame, bbox)
