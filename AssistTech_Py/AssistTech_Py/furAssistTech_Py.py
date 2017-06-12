f06accd1f58cb1ab11cdeeac06ecbc9b062c4327
fk2969 <furkan.kinli@ozu.edu.tr>
2017-06-12 12:53:05 +0300

    .

--- AssistTech_Py/AssistTech_Py/furAssistTech_Py.py
+++ AssistTech_Py/AssistTech_Py/furAssistTech_Py.py
@@ -23,33 +23,24 @@ def detect_face(frame):
 
 def main():
     tracker = cv2.Tracker_create("KCF")
-    c = input("Face detection (1) vs. Foreground information (2):")
+    c = input("Face detection (1) vs. Foreground information (2) vs. Simple Blob Detection (3):")
     video = cv2.VideoCapture(0)
     element = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
     mog2_bgs = cv2.createBackgroundSubtractorMOG2()
-    # Setup SimpleBlobDetector parameters.
     params = cv2.SimpleBlobDetector_Params()
 
-    # Change thresholds
     params.minThreshold = 10
     params.maxThreshold = 200
-
-    # Filter by Area.
     params.filterByArea = True
     params.minArea = 500
-
-    # Filter by Circularity
     params.filterByCircularity = True
     params.minCircularity = 0.1
-
-    # Filter by Convexity
     params.filterByConvexity = False
     params.minConvexity = 0.87
-
-    # Filter by Inertia
     params.filterByInertia = False
     params.minInertiaRatio = 0.01
     detector = cv2.SimpleBlobDetector_create(params)
+
     first_frame = True
     bbox = (0,0,0,0)
 
@@ -93,11 +84,13 @@ def main():
                                 largest_area = area
                                 first_frame = False
             elif c == "3":
-
                 keypoints = detector.detect(frame)
-                im_with_keypoints = cv2.drawKeypoints(frame, keypoints, np.array([]), (0, 0, 255),
-                                                      cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-                cv2.imshow('AssistTech', im_with_keypoints)
+                if len(keypoints) > 0:
+                    bbox = (int(keypoints[0].pt[0]),int(keypoints[0].pt[1]),int(keypoints[0].size),int(keypoints[0].size))
+                    print(bbox)
+                    first_frame = False
+                else: print("%s" % str(keypoints))
+
             else:
                 print("Wrong input!")
         else:
@@ -109,7 +102,7 @@ def main():
                 p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
                 cv2.rectangle(frame, p1, p2, (0, 0, 255), 2)
 
-        #cv2.imshow('AssistTech', frame)
+        cv2.imshow('AssistTech', frame)
 
         k = cv2.waitKey(1) & 0xff
         if k == 27:
