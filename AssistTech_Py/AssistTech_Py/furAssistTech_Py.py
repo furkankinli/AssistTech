d4eb646867d13e3ddb5501c59c037c1fb000b81d
fk2969 <furkan.kinli@ozu.edu.tr>
2017-06-15 16:14:01 +0300

    .

--- AssistTech_Py/AssistTech_Py/furAssistTech_Py.py
+++ AssistTech_Py/AssistTech_Py/furAssistTech_Py.py
@@ -6,18 +6,13 @@ import pyautogui
 
 def detect_face(frame):
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    gray = cv2.equalizeHist(gray)
+    faces = face_cascade.detectMultiScale(gray, 1.4, 3)
     for (x, y, w, h) in faces:
         frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
         bbox = (x, y, w, h)
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = frame[y:y + h, x:x + w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
     return bbox
 
 
